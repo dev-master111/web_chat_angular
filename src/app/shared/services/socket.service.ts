4c196c6b80011836ca5853f50bed165512dad9a0
dev-master111 <dev.master101@gmail.com>
2018-09-17 10:58:53 +0800

    fix: changed socket.io listening port

--- src/app/shared/services/socket.service.ts
+++ src/app/shared/services/socket.service.ts
@@ -12,7 +12,7 @@ export class SocketService {
   }
 
   initiateSocket() {
-    this.socket = io.connect('https://webchat18api.herokuapp.com:8080', { secure: true, reconnect: true, rejectUnauthorized : false });
+    this.socket = io.connect('wss://webchat18api.herokuapp.com:3001', { secure: true, reconnect: true, rejectUnauthorized : false });
     // this.socket = io.connect('http://localhost:8080', { secure: true, reconnect: true, rejectUnauthorized : false });
   }
 
